80e6d9d77276141999b00cdaf39ea8fdbe043976
Alexandre Vasseur <avasseur@vmware.com>
2016-06-20 15:02:37 +0200

    2.4

--- buildv2.sh
+++ buildv2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-sed -e "s/3EB0FF/3EBF5C/" -i "" src/main/resources/templates/fragments/layout.html
-sed -e "s/<version>1.0/<version>2.0/" -i "" pom.xml
+set -e -x
+
+sed -e "s/3EB0FF/3EBF5C/" --in-place src/main/resources/templates/fragments/layout.html
+sed -e "s/<version>1.0/<version>2.0/" --in-place pom.xml
 
 mvnw package -DskipTests
 
-sed -e "s/3EBF5C/3EB0FF/" -i "" src/main/resources/templates/fragments/layout.html
-sed -e "s/<version>2.0/<version>1.0/" -i "" pom.xml
+sed -e "s/3EBF5C/3EB0FF/" --in-place src/main/resources/templates/fragments/layout.html
+sed -e "s/<version>2.0/<version>1.0/" --in-place pom.xml
 
